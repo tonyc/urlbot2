8f96de5a8c02d17fc1dc577ce3e056aab68ade0a
Tony Collen <tony@recursiveawesome.com>
2011-06-29 15:54:51 -0500

    fix spaces, this ain't python

--- urlbot.rb
+++ urlbot.rb
@@ -16,23 +16,23 @@ bot = Cinch::Bot.new do
   on :channel do |m|
     URI.extract(m.message, "http").each do |url|
       if content_type_from_head(url) =~ %r{text/html}
-				m.reply get_title(url)
-			end
-		end
+        m.reply get_title(url)
+      end
+    end
   end
   
   helpers do
     def get_title(url)
       doc = Hpricot(open(url))
-			return (doc/'title').text.strip.split.join(" ")
+      return (doc/'title').text.strip.split.join(" ")
     end
     
     def content_type_from_head(raw_uri)
-  		uri = URI.parse(raw_uri)
-  		http = Net::HTTP.new(uri.host, uri.port)
-  		response, data = http.head((uri.path == "" ? "/" : uri.path), nil)
-  		return response.content_type
-  	end
+      uri = URI.parse(raw_uri)
+      http = Net::HTTP.new(uri.host, uri.port)
+      response, data = http.head((uri.path == "" ? "/" : uri.path), nil)
+      return response.content_type
+    end
   end
   
 end
